3e4ab39d5269d0d105d221c04e7a61baa0ff2981
Arzza Munabim <aprilabim2804@gmail.com>
2025-04-30 01:25:49 +0700

    Mengedit Animation di Swiper

--- src/pages/user/HomePage.jsx
+++ src/pages/user/HomePage.jsx
@@ -4,32 +4,50 @@ import { useParams } from "react-router";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { ProductCard } from "../../components/card/product-card";
+import { Autoplay } from "swiper/modules";
 export const HomePage = () => {
   const { keyword } = useParams();
 
   return (
     <>
-    <section className="p-10 bg-pink-50">
-      {/* Ini adalah Teks */}
-      
-      <div className="">
-        <h1>77TopUp</h1>
-        <h1>Ini adalah website 77TopUp</h1>
-        <InputSearch/>
+    {/* Hero Section */}
+    <section className="bg-pink-200 p-10 py-16 px-10">
+        <div className="flex items-center justify-between gap-10">
       </div>
+
+      {/* LEFT - Text */}
+      <div className="w-1/2">
+      <p className="text-blue-500 font-medium uppercase mb-2">Welcome to 77TopUp</p>
+            <h1 className="text-4xl font-extrabold text-gray-900 mb-4">Topup Murah Proses Cepat</h1>
+            <p className="text-gray-700 mb-6">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus, aut esse aliquam labore voluptatem cumque laborum. Fuga reiciendis ut laboriosam aut iste facilis voluptates officiis, incidunt itaque nihil nostrum similique!
+            </p>
+            <div className="w-full">
+              <InputSearch />
+            </div>
+          </div>
   
-      {/* Image Swiper */}
-      <div>
+      {/* RIGHT - IMAGE SWIPER */}
+      <div className="w-1/2">
       <Swiper
-        spaceBetween={50}
-        slidesPerView={3}
+        modules={[Autoplay]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop = {true}
+        speed={8000}
+        autoplay = {{
+          delay : 10000,
+          disableOnInteraction: false,
+          reverseDirection: false,
+          pauseOnMouseEnter: false, 
+        }}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
-      <SwiperSlide><img src="/77topup_logo_1b.png" alt="" /></SwiperSlide>
-      <SwiperSlide><img src="/77topup_logo_1b.png" alt="" /></SwiperSlide>
-      <SwiperSlide><img src="/77topup_logo_1b.png" alt="" /></SwiperSlide>
-      <SwiperSlide><img src="/77topup_logo_1b.png" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="/77topup_logo_1b.png" alt="" className="w-full"/></SwiperSlide>
+      <SwiperSlide><img src="/77topup_logo_1b.png" alt="" className="w-full"/></SwiperSlide>
+      <SwiperSlide><img src="/77topup_logo_1b.png" alt="" className="w-full"/></SwiperSlide>
+      <SwiperSlide><img src="/77topup_logo_1b.png" alt="" className="w-full"/></SwiperSlide>
       </Swiper>
       </div>
     </section>
