9cf724cf9672cd0285d8b1041c17f63fa986b8bf
Arzza Munabim <aprilabim2804@gmail.com>
2025-05-02 08:26:03 +0700

    Menambahkan Produk Card

--- src/pages/user/HomePage.jsx
+++ src/pages/user/HomePage.jsx
@@ -11,11 +11,10 @@ export const HomePage = () => {
   return (
     <>
     {/* Hero Section */}
-    <section className="bg-pink-200 p-10 py-16 px-10">
-  {/* Tambahkan elemen teks dan gambar ke dalam flex container */}
+  <section className="bg-pink-200 p-10 py-16 px-10">
   <div className="flex items-center justify-between gap-10">
     {/* LEFT - Text */}
-    <div className="w-1/2">
+    <div className="flex flex-wrap ">
       <p className="text-blue-500 font-medium uppercase mb-2">Welcome to 77TopUp</p>
       <h1 className="text-4xl font-extrabold text-gray-900 mb-4">Topup Murah Proses Cepat</h1>
       <p className="text-gray-700 mb-6">
@@ -28,7 +27,7 @@ export const HomePage = () => {
     </div>
 
     {/* RIGHT - IMAGE SWIPER */}
-    <div className="w-1/2">
+    <div className="flex items-center justify-between w-1/2">
       <Swiper
         modules={[Autoplay]}
         spaceBetween={30}
@@ -54,16 +53,36 @@ export const HomePage = () => {
 
 
     {/* Product Card */}
-    <section className="p-10 bg-blue-300">
-   <div className="container mx-auto px-4">
-    <div className="text-xl font-semibold">Ini Adalah Game</div>
-    <div className="grid grid-cols-3 gap-5 mt-5">
+    <section className="p-10 bg-white min-h-screen">
+  <div className="container mx-auto px-4">
+    
+    {/* Judul/Kategori */}
+    <div className="flex flex-wrap gap-4 mb-8">
+      <div className="bg-blue-500 text-white font-semibold px-4 py-2 rounded">
+        Game Via ID 24 JAM
+      </div>
+    </div>
+
+    {/* Judul
+    <div className="text-xl font-semibold mb-4">Ini Adalah Game</div> */}
+
+    {/* Card Produk */}
+    <div className="flex flex-wrap gap-4 mb-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
         <ProductCard />
         <ProductCard />
         <ProductCard />
       </div>
-      </div>
-    </section>
+    </div>
+  </div>
+</section>
+
+
     </>
   );
 };
